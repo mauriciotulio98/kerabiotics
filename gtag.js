bef241314b25e26b71559854fab7f2534a2ac244
mauriciotulio98 <mauriciotulio98@gmail.com>
2024-12-28 13:54:04 -0500

    Create gtag.js

---
+++ gtag.js
@@ -0,0 +1,10 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-11522447433">
+</script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-11522447433');
+</script>
